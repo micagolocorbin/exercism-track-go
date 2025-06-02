f76be9dbe55c3aee723dd1930729bb6b4a30f207
micagolocorbin <micagolocorbin@gmail.com>
2025-06-02 14:09:49 +0200

    solution for 21st exercise

---
+++ airport-robot/airport_robot.go
@@ -0,0 +1,39 @@
+package airportrobot
+
+import "fmt"
+
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+type German struct{}
+
+func (g German) LanguageName() string {
+	return "German"
+}
+func (g German) Greet(name string) string {
+	return fmt.Sprintf("Hallo %s!", name)
+}
+
+type Italian struct{}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+func (i Italian) Greet(name string) string {
+	return fmt.Sprintf("Ciao %s!", name)
+}
+
+type Portuguese struct{}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+func (p Portuguese) Greet(name string) string {
+	return fmt.Sprintf("Olá %s!", name)
+}
+
+func SayHello(name string, g Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", g.LanguageName(), g.Greet(name))
+}
